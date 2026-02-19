df26740e83f7f49a4d9d0e3b7dc5402d3d1446e1
John Everton <johneverton44@gmail.com>
2026-02-19 11:48:01 -0300

    feat: create accept invite route

---
+++ apps/api/src/http/routes/invites/accept-invite.ts
@@ -0,0 +1,73 @@
+import { authMiddleware } from '@/http/middlewares/auth'
+import { prisma } from '@/lib/prisma'
+import type { FastifyInstance } from 'fastify'
+import type { ZodTypeProvider } from 'fastify-type-provider-zod'
+import { z } from 'zod'
+import { BadRequestError } from '../_errors/bad-request-error'
+
+export function acceptInvite(app: FastifyInstance) {
+  app
+    .withTypeProvider<ZodTypeProvider>()
+    .register(authMiddleware)
+    .post(
+      '/invite/:inviteId/accept',
+      {
+        schema: {
+          tags: ['Invites'],
+          summary: 'Accept an invite',
+          params: z.object({
+            inviteId: z.uuid(),
+          }),
+          response: {
+            204: z.null(),
+          },
+        },
+      },
+      async (request, reply) => {
+        const userId = await request.getCurrentUserUserId()
+        const { inviteId } = request.params
+
+        const invite = await prisma.invite.findUnique({
+          where: {
+            id: inviteId,
+          },
+        })
+
+        if (!invite) {
+          throw new BadRequestError('Invite not found.')
+        }
+
+        const user = await prisma.user.findUnique({
+          where: {
+            id: userId,
+          },
+        })
+
+        if (!user) {
+          throw new BadRequestError('User not found.')
+        }
+
+        if (invite.email !== user.email) {
+          throw new BadRequestError('This invite belongs to another user')
+        }
+
+        await prisma.$transaction([
+          prisma.member.create({
+            data: {
+              userId,
+              organizationId: invite.organizationId,
+              role: invite.role,
+            },
+          }),
+
+          prisma.invite.delete({
+            where: {
+              id: inviteId,
+            },
+          }),
+        ])
+
+        return reply.status(204).send(null)
+      }
+    )
+}
