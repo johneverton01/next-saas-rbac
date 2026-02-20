a9f2d25f82ac46cfc1aba92ebeec3e8fc6be595a
John Everton <johneverton44@gmail.com>
2026-02-20 16:30:56 -0300

    chore: config shadcn components

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -11,7 +11,7 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="pt-BR">
+    <html lang="pt-BR" className="dark">
       <body className="antialiased">{children}</body>
     </html>
   )
