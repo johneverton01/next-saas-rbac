eb72ab12e66c82a49c43d0c06a7317396e6337a8
John Everton <johneverton44@gmail.com>
2026-02-22 21:03:44 -0300

    feat: create organization swicher

--- apps/web/src/components/header.tsx
+++ apps/web/src/components/header.tsx
@@ -1,11 +1,17 @@
-import { Laptop2 } from 'lucide-react'
+import { Laptop2, Slash } from 'lucide-react'
+import { OrganizationSwitcher } from './organization-switcher'
 import { ProfileButton } from './profile-button'
 
 export function Header() {
   return (
     <header className="mx-auto flex max-w-300 items-center justify-between">
       <div className="flex items-center gap-3">
-        <Laptop2 className="size-6 dark:invert" />
+        <div>
+          <Laptop2 className="light:invert size-6" />
+        </div>
+
+        <Slash className="text-muted-foreground size-3 -rotate-24" />
+        <OrganizationSwitcher />
       </div>
 
       <div className="flex items-center gap-4">
